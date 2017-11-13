9555f368df5764c0c1a0e87a14bab72d66eb74d5
Jitin Pillai <jitinpillai@Jitins-Mac-Pro.local>
2017-11-13 17:13:18 -0500

    Pass ICO Symbol and fetch ERC20 Address from etherscan.io networks.
    Needs to fix: Parsing the response page for Address

---
+++ controller/ethScan.js
@@ -0,0 +1,42 @@
+var request = require('request');
+var cheerio = require('cheerio');
+
+module.exports = {
+
+    erc20Token: function (id) {
+
+        // Sample data-structure for indexing values
+        // data = {
+        //     "id": symbol,
+        //     "url": url,
+        //     "concept": concept,
+        //     "contents": [
+        //         {
+        //             "title": home_page_title,
+        //             "data": home_content
+        //         },
+        //         {
+        //             "title": team_page_title,
+        //             "data": team_page_content
+        //         }
+        //     ]
+        // };
+
+        var ethScanWebsite = 'https://etherscan.io/' + 'search/q=' + id;
+
+        request(ethScanWebsite, function (error, response, html) {
+            if (!error && response.statusCode == 200) {
+                // console.log(html);
+                var $ = cheerio.load(html);
+
+                var data = {};
+
+                var _address = $('#ContentPlaceHolder1_trContract').children().;
+                console.log(_address);
+
+
+            }
+        });
+        return;
+    }
+}
---
+++ index.js
@@ -0,0 +1,11 @@
+var fs = require('fs');
+var ethScan = require('./controller/ethScan');
+
+var icoList = JSON.parse(fs.readFileSync('ico.json', 'utf8'));
+
+icoList.forEach(function (item) {
+    var id = item.symbol;
+    console.log(id);
+    var erc20Token = ethScan.erc20Token(id);
+    // console.log(erc20Token);
+});
